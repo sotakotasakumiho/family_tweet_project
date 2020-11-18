2f596bfeaef739a0d848c3e311916091b7f29b30
塩見聡太朗 <shiomisatoshitarou@shiomisatoshitarounoMacBook-Pro.local>
2020-11-19 00:52:12 +0900

    commit

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
       get "login" => "users#login_form"
       post "login" => "users#login"
       post "logout" => "users#logout"
-  
+
 
   resources :posts do
     resources :likes, only: [:index, :create, :destroy]
